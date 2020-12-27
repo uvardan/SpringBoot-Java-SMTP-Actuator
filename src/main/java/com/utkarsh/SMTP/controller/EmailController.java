78e45123770873dc60f65198185303160c5e1d42
uvardan <uvardan@utexas.edu>
2020-12-27 16:50:11 -0600

    POC changes

---
+++ src/main/java/com/utkarsh/SMTP/controller/EmailController.java
@@ -0,0 +1,19 @@
+package com.utkarsh.SMTP.controller;
+
+import com.utkarsh.SMTP.service.SendEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    SendEmailService sendEmailService;
+
+
+    @RequestMapping(value = "/sendemail")
+    public void sendEmail() {
+        sendEmailService.sendEmail("uvardan@hotmail.com","Hello testing", "SMTP");
+    }
+}
---
+++ src/main/java/com/utkarsh/SMTP/service/SendEmailService.java
@@ -0,0 +1,27 @@
+package com.utkarsh.SMTP.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendEmailService {
+
+
+    @Autowired
+    public JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String body, String topic){
+
+        System.out.println("#################################### Sending email ##################################");
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("uvardan427@gmail.com");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(topic);
+        simpleMailMessage.setText(body);
+        javaMailSender.send(simpleMailMessage);
+        System.out.println("#################################### SENT ###########################################");
+
+    }
+}
